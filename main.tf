aed870c79734cedaded9575097990fe675d2598d
Tyler Wenzel <tyler.gh3@gmail.com>
2024-03-07 22:07:47 -0500

    begin provisioning of s3 bucket and firehose
    Co-authored-by: Brandon-Corey <bcorey3660@gmail.com>
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>

--- main.tf
+++ main.tf
@@ -40,6 +40,21 @@ resource "aws_apigatewayv2_stage" "api_stage" {
   auto_deploy = true
 }
 
+resource "aws_iam_policy" "lambda_managed_policy" {
+  name = "lambda-firehose-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["firehose:*"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 # Example IAM Role for Lambda
 resource "aws_iam_role" "lambda_iam_role" {
   name = "lambda_iam_role"
@@ -53,6 +68,7 @@ resource "aws_iam_role" "lambda_iam_role" {
       Action = "sts:AssumeRole"
     }]
   })
+  managed_policy_arns = [aws_iam_policy.lambda_managed_policy.arn]
 }
 
 resource "aws_lambda_permission" "allow_api" {
@@ -62,3 +78,58 @@ resource "aws_lambda_permission" "allow_api" {
   principal           = "apigateway.amazonaws.com"
   source_arn          = "${aws_apigatewayv2_api.event_gateway.execution_arn}/*/*/events"
 }
+
+// Firehose
+resource "aws_kinesis_firehose_delivery_stream" "events_firehose" {
+  name        = "terraform-kinesis-firehose-extended-s3-test-stream"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.events_bucket.arn
+  }
+}
+
+data "aws_iam_policy_document" "firehose_assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+
+  }
+}
+
+resource "aws_iam_policy" "firehose_managed_policy" {
+  name = "firehose-managed-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+        "s3:PutObject"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name                = "firehose_test_role"
+  assume_role_policy  = data.aws_iam_policy_document.firehose_assume_role_policy.json
+  managed_policy_arns = [aws_iam_policy.firehose_managed_policy.arn]
+}
+
+// S3
+resource "aws_s3_bucket" "events_bucket" {}
