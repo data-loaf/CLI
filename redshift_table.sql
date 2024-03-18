3fe96a9af869ad35074df5b8bba466a8896c6168
Brandon Corey <bcorey3660@gmail.com>
2024-03-18 02:35:06 -0400

    change column order of table schemas to be more intuitive

--- redshift_table.sql
+++ redshift_table.sql
@@ -1,16 +1,15 @@
 CREATE TABLE public.users (
     id               INTEGER IDENTITY(1,1) PRIMARY KEY,
     user_id          VARCHAR(50) NOT NULL,
-    user_created     DATETIME DEFAULT SYSDATE,
-    user_attributes  SUPER 
+    user_attributes  SUPER,
+    user_created     TIMESTAMP DEFAULT SYSDATE
 );
 
 CREATE TABLE public.events (   
-   
     id                INTEGER IDENTITY(1,1) PRIMARY KEY,
     event_id          VARCHAR(50) NOT NULL,
     event_name        VARCHAR(255),
-    user_id           INTEGER REFERENCES Users(id),
-    event_created     DATETIME DEFAULT SYSDATE,
-    event_attributes  SUPER -- Use SUPER data type for JSON
+    user_id           VARCHAR(50) REFERENCES Users(id),
+    event_attributes  SUPER, -- Use SUPER data type for JSON
+    event_created     TIMESTAMP DEFAULT SYSDATE
 );
