c204af0ac87ca8b4f55898841df7347c8753748d
Brandon Corey <bcorey3660@gmail.com>
2024-03-17 20:38:06 -0400

    rearrange tables to be created in correct order

--- redshift_table.sql
+++ redshift_table.sql
@@ -1,15 +1,16 @@
-CREATE TABLE public.events (
+CREATE TABLE public.users (
+    id               INTEGER IDENTITY(1,1) PRIMARY KEY,
+    user_id          VARCHAR(50) NOT NULL,
+    user_created     DATETIME DEFAULT SYSDATE,
+    user_attributes  SUPER 
+);
+
+CREATE TABLE public.events (   
+   
     id                INTEGER IDENTITY(1,1) PRIMARY KEY,
     event_id          VARCHAR(50) NOT NULL,
     event_name        VARCHAR(255),
     user_id           INTEGER REFERENCES Users(id),
     event_created     DATETIME DEFAULT SYSDATE,
     event_attributes  SUPER -- Use SUPER data type for JSON
-);
-
-CREATE TABLE public.users (
-    id               INTEGER IDENTITY(1,1) PRIMARY KEY,
-    user_id          VARCHAR(50) NOT NULL,
-    user_created     DATETIME DEFAULT SYSDATE,
-    user_attributes  SUPER 
 );
