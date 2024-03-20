42bb18814ebd0f2dbdd459e65b4f3056ba4434a7
Tyler Wenzel <tyler.gh3@gmail.com>
2024-03-19 21:36:00 -0400

    add ec2 provision with demo application deployment
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

---
+++ ec2.tf
@@ -0,0 +1,105 @@
+/*
+Things to execute on provision
+1. install node
+2. install postgres
+2a. figure out when to create tables
+3. clone repo
+4. start app server
+
+ssh into ec2?
+
+things to do:
+
+how do we determine the region needed for the AMI
+where do we want to store the pem key
+
+*/
+
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${var.key_name}"
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "loaf_sg_ec2" {
+  name        = "loaf_sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "loaf_application" {
+  ami                    = "ami-0f8b8f874036055b1"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.loaf_sg_ec2.id]
+
+  tags = {
+    Name = "loaf_application"
+  }
+}
+
+resource "null_resource" "remote" {
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/")
+    host        = aws_instance.loaf_application.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y nodejs npm",
+      "git clone https://github.com/CodeSagarOfficial/nodejs-demo.git",
+      "cd nodejs-demo",
+      "sudo npm install -g pm2",
+      "npm install",
+      "pm2 start app.js"
+
+    ]
+  }
+}
+
+
+
