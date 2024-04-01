b1afc913a3c4f4d0ca0bf2fe9824deec356bcdd3
Tyler Wenzel <tyler.gh3@gmail.com>
2024-04-01 13:53:49 -0400

    implement lambda resource for user updates
    Co-authored-by: Brandon-Corey <bcorey3660@gmail.com>
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,26 @@
 # Example Lambda Function
 resource "aws_lambda_function" "stream_router_lambda" {
-  filename      = "stream_router_lambda.zip"
+  filename      = "./lib/stream_router_lambda.zip"
   function_name = "stream_router"
   role          = aws_iam_role.lambda_iam_role.arn
   handler       = "index.handler"
   runtime       = "nodejs20.x"
+}
+
+# Example Lambda Function
+resource "aws_lambda_function" "update_user_lambda" {
+
+  filename      = "./lib/lambda_user_update.zip"
+  function_name = "update_user"
+  role          = aws_iam_role.lambda_iam_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+
+  environment {
+    variables = {
+      REDSHIFT_CONN_STRING = "postgresql://${aws_redshift_cluster.redshift_cluster.master_username}:${aws_redshift_cluster.redshift_cluster.master_password}@${aws_redshift_cluster.redshift_cluster.endpoint}/${aws_redshift_cluster.redshift_cluster.database_name}"
+    }
+  }
 }
 
 resource "aws_iam_policy" "lambda_managed_policy" {
@@ -52,6 +68,14 @@ resource "aws_lambda_permission" "allow_api_users" {
   function_name       = aws_lambda_function.stream_router_lambda.function_name
   principal           = "apigateway.amazonaws.com"
   source_arn          = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/POST/users"
+}
+
+resource "aws_lambda_permission" "allow_api_update_users" {
+  statement_id_prefix = "ExecuteByAPI"
+  action              = "lambda:InvokeFunction"
+  function_name       = aws_lambda_function.update_user_lambda.function_name
+  principal           = "apigateway.amazonaws.com"
+  source_arn          = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/POST/update"
 }
 
 
