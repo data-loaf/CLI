17ba50599c7a75658e8f1889b065e180a65ee522
Tyler Wenzel <tyler.gh3@gmail.com>
2024-04-01 14:38:59 -0400

    delete resource code that is no longer needed

--- ec2.tf
+++ ec2.tf
@@ -64,46 +64,6 @@ resource "aws_instance" "loaf_app" {
   }
 }
 
-/* resource "aws_acm_certificate" "cert" {
-  domain_name       = aws_instance.loaf_dev.public_dns
-  validation_method = "DNS"
-
-  tags = {
-    Environment = "prod"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-data "aws_route53_zone" "loaf_app_zone" {
-  name         = aws_instance.loaf_dev.public_dns
-  private_zone = false
-}
-
-resource "aws_route53_record" "loaf_app_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.loaf_app_zone.zone_id
-}
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.loaf_app_record : record.fqdn]
-}
-*/
 output "ec2_url" {
   value = aws_instance.loaf_app.public_dns
 } 
