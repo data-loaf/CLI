535a0daeaa8bd1b7b12c8ea2299590367d484e09
Brandon Corey <bcorey3660@gmail.com>
2024-04-04 20:26:18 -0400

    allow for simple multiform input and retrieval of values
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- golang/cmd/deploy.go
+++ golang/cmd/deploy.go
@@ -1,31 +1,51 @@
 package cmd
 
 import (
+	inputs "dataloaf/cmd/inputs"
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
-func getAccessKey() {}
+func mergeFlagsAndInputs(
+	flags inputs.TextInputFields,
+	inputs inputs.TextInputFields,
+) inputs.TextInputFields {
+	mergedInputs := make(map[string]string)
 
-func getSecretKey() {}
+	for key, value := range flags {
+		mergedInputs[key] = value
+	}
 
-func getRegion() {}
+	for key, value := range inputs {
+		mergedInputs[key] = value
+	}
+
+	return mergedInputs
+}
 
 func executeDeploy(cmd *cobra.Command, args []string) {
 	accessKey, _ := cmd.Flags().GetString("access")
 	secretKey, _ := cmd.Flags().GetString("secret")
-	region, _ := cmd.Flags().GetString("region")
+	// region, _ := cmd.Flags().GetString("region")
 
-	if accessKey == "" {
-		getAccessKey()
+	flags := inputs.TextInputFields{"AccessKey": accessKey, "SecretKey": secretKey}
+	fmt.Println(flags)
+	model := inputs.InitialModel(flags)
+
+	if accessKey == "" || secretKey == "" {
+		if _, err := tea.NewProgram(model).Run(); err != nil {
+			fmt.Printf("could not start program: %s\n", err)
+			os.Exit(1)
+		}
 	}
 
-	if secretKey == "" {
-		getSecretKey()
-	}
+	resultInputs := inputs.TextResults
+	mergedResult := mergeFlagsAndInputs(flags, resultInputs)
 
-	if region == "" {
-		getRegion()
-	}
+	fmt.Println(mergedResult)
 }
 
 // deployCmd represents the deploy command
---
+++ golang/cmd/inputs/text-inputs.go
@@ -0,0 +1,188 @@
+package inputs
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle.Copy()
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle.Copy()
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Copy().Render("[ Continue ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Continue"))
+)
+
+var TextResults TextInputFields
+
+type TextInputFields map[string]string
+
+type model struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode cursor.Mode
+}
+
+func (m model) GetInputValues() TextInputFields {
+	values := make(TextInputFields, 2)
+
+	for _, input := range m.inputs {
+		switch input.Placeholder {
+		case "Access Key":
+			values["AccessKey"] = input.Value()
+		case "Secret Key":
+			values["SecretKey"] = input.Value()
+		}
+	}
+	return values
+}
+
+func createTextInput(placeHolder string, inputs []textinput.Model) textinput.Model {
+	keyInput := textinput.New()
+	keyInput.Cursor.Style = cursorStyle
+	keyInput.CharLimit = 64
+	keyInput.Placeholder = placeHolder
+
+	if len(inputs) == 0 {
+		keyInput.Focus()
+		keyInput.PromptStyle = focusedStyle
+		keyInput.TextStyle = focusedStyle
+	}
+
+	return keyInput
+}
+
+func InitialModel(flags TextInputFields) model {
+	var inputs []textinput.Model
+
+	if flags["AccessKey"] == "" {
+		accessKeyInput := createTextInput("Access Key", inputs)
+		inputs = append(inputs, accessKeyInput)
+	}
+
+	if flags["SecretKey"] == "" {
+		secretKeyInput := createTextInput("Secret Key", inputs)
+		inputs = append(inputs, secretKeyInput)
+	}
+
+	return model{
+		inputs:     inputs,
+		focusIndex: 0,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		// Change cursor mode
+		case "ctrl+r":
+			m.cursorMode++
+			if m.cursorMode > cursor.CursorHide {
+				m.cursorMode = cursor.CursorBlink
+			}
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				cmds[i] = m.inputs[i].Cursor.SetMode(m.cursorMode)
+			}
+			return m, tea.Batch(cmds...)
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				inputValues := m.GetInputValues()
+				TextResults = inputValues
+				return m, tea.Quit
+			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					// Set focused state
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
+		}
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	b.WriteString(helpStyle.Render("cursor mode is "))
+	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
+	b.WriteString(helpStyle.Render(" (ctrl+r to change style)"))
+
+	return b.String()
+}
