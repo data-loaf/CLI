52d61283d749d003bbdfce032a2ebfac4ef9e99f
Tyler Wenzel <tyler.gh3@gmail.com>
2024-04-08 23:10:41 -0400

    add load balancer and associated certificate provisioning
    Co-authored-by: Brandon-Corey <bcorey3660@gmail.com>
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,41 +1,129 @@
-resource "aws_elb" "bar" {
-  name               = "foobar-terraform-elb"
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  /*   listener {
-    instance_port      = 8000
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
-  } */
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  instances                   = [aws_instance.loaf_app.id]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-
+resource "aws_default_vpc" "default" {
   tags = {
-    Name = "dataloaf_lb"
+    Name = "Default"
   }
 }
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "Default subnet 1"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "Default subnet 2"
+  }
+}
+
+resource "aws_security_group" "loaf_sg_lb" {
+  name        = "loaf_sg_lb"
+  description = "Security group for Load Balancer"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.loaf_sg_lb.id]
+  subnets            = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "loaf_listener" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.loaf_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "loaf_tg" {
+  name     = "dataloaf-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+
+  health_check {
+    enabled           = true
+    healthy_threshold = 3
+    matcher           = 200
+    path              = "/health"
+    protocol          = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment_a" {
+  target_group_arn = aws_lb_target_group.loaf_tg.arn
+  target_id        = aws_instance.loaf_app.id
+}
+
+# ********************************* #
+# * ACM *                           #
+# ********************************* #
+resource "aws_route53_zone" "primary" {
+  name = var.domain_name
+}
+
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_dns" {
+  for_each = {
+    for robo in aws_acm_certificate.certificate.domain_validation_options : robo.domain_name => {
+      name   = robo.resource_record_name
+      record = robo.resource_record_value
+      type   = robo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.primary.zone_id
+}
+
+resource "aws_acm_certificate_validation" "certificate" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_dns : record.fqdn]
+}
+
 output "load_balancer_dns" {
-  value = aws_elb.bar.dns_name
+  value = aws_lb.test.dns_name
 }
