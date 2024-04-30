d8ea6ccfaa4a0aa6bbeaf67d3dd8a2e0a03d4ef1
Brandon Corey <bcorey3660@gmail.com>
2024-04-30 01:24:47 -0400

    migrates http api to rest api
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- terraform/http/api_gateway.tf
+++ terraform/http/api_gateway.tf
@@ -1,35 +1,128 @@
-resource "aws_apigatewayv2_api" "api_gateway" {
-  name          = var.api_gateway_name
-  protocol_type = var.api_gateway_protocol
-  tags = {
-    app: "dataloaf"
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = var.api_gateway_name
+  description = "API gateway for dataloaf"
+}
+
+resource "aws_api_gateway_resource" "events" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "events"
+}
+
+resource "aws_api_gateway_resource" "users" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "users"
+}
+
+resource "aws_api_gateway_resource" "update_users" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "users" // Adjust the path_part according to your API path
+}
+
+resource "aws_api_gateway_method" "post_events" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id  = aws_api_gateway_resource.events.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "post_users" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id  = aws_api_gateway_resource.users.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "patch_users" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id  = aws_api_gateway_resource.update_users.id
+  http_method   = "PATCH"
+  authorization = "NONE"
+}
+
+resource "aws_iam_role" "api_gateway_role" {
+  name               = "api_gateway_kinesis_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  // Add policies for accessing Kinesis here if not already added
+}
+
+resource "aws_api_gateway_integration" "events_kinesis_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id            = aws_api_gateway_resource.events.id
+  http_method             = aws_api_gateway_method.post_events.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.region}:kinesis:action/PutRecord"
+  credentials             = aws_iam_role.api_gateway_role.arn
+}
+
+resource "aws_api_gateway_integration" "users_kinesis_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id            = aws_api_gateway_resource.users.id
+  http_method             = aws_api_gateway_method.post_users.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.region}:kinesis:action/PutRecord"
+  credentials             = aws_iam_role.api_gateway_role.arn
+}
+
+resource "aws_api_gateway_integration" "update_user_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id            = aws_api_gateway_resource.update_users.id
+  http_method             = aws_api_gateway_method.patch_users.http_method
+  integration_http_method = "PATCH"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.update_user_lambda.invoke_arn
+}
+
+# Integration responses for events Kinesis integration
+resource "aws_api_gateway_integration_response" "events_kinesis_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.events.id
+  http_method = aws_api_gateway_method.post_events.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
   }
 }
 
-resource "aws_apigatewayv2_integration" "update_user_lambda_integration" {
-  api_id           = aws_apigatewayv2_api.api_gateway.id
-  integration_type = var.api_gateway_integration_type
-
-  connection_type      = var.api_gateway_connection_type
-  description          = var.api_gateway_integration_description
-  integration_method   = var.api_gateway_integration_method
-  integration_uri      = aws_lambda_function.update_user_lambda.invoke_arn
-  passthrough_behavior = var.api_gateway_passthrough_behavior
+# Method responses for events endpoint
+resource "aws_api_gateway_method_response" "events_method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.events.id
+  http_method = aws_api_gateway_method.post_events.http_method
+  status_code = "200"
 }
 
-resource "aws_apigatewayv2_route" "update_users_route" {
-  api_id    = aws_apigatewayv2_api.api_gateway.id
-  route_key = var.api_gateway_users_update_route_key
-  target    = "integrations/${aws_apigatewayv2_integration.update_user_lambda_integration.id}"
+# Integration responses for users Kinesis integration
+resource "aws_api_gateway_integration_response" "users_kinesis_integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = aws_api_gateway_method.post_users.http_method
+  status_code = "200"
+
+  response_templates = {
+    "application/json" = ""
+  }
 }
 
-resource "aws_apigatewayv2_stage" "api_stage" {
-  api_id = aws_apigatewayv2_api.api_gateway.id
-  name   = var.api_gateway_stage_name
-
-  auto_deploy = true
-}
-
-output "api_endpoint" {
-  value = aws_apigatewayv2_api.api_gateway.api_endpoint
+# Method responses for users endpoint
+resource "aws_api_gateway_method_response" "users_method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = aws_api_gateway_method.post_users.http_method
+  status_code = "200"
 }
--- terraform/http/lambda.tf
+++ terraform/http/lambda.tf
@@ -1,12 +1,10 @@
-# Data Update Lambda Function
 resource "aws_lambda_function" "update_user_lambda" {
-
   filename      = "./lib/lambda_user_update.zip"
   function_name = "update_user"
   role          = aws_iam_role.lambda_iam_role.arn
   handler       = "index.handler"
   runtime       = "nodejs20.x"
-  timeout = 60
+  timeout       = 60
 
   environment {
     variables = {
@@ -15,11 +13,12 @@ resource "aws_lambda_function" "update_user_lambda" {
   }
 }
 
+# IAM Policy for Lambda
 resource "aws_iam_policy" "lambda_policy" {
   name = "lambda-policy"
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
         Action   = ["firehose:*"]
@@ -32,26 +31,28 @@ resource "aws_iam_policy" "lambda_policy" {
 
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_iam_role" {
-  name = "lambda_iam_role"
+  name               = "lambda_iam_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
+
   managed_policy_arns = [aws_iam_policy.lambda_policy.arn]
 }
 
-resource "aws_lambda_permission" "allow_api_update_users" {
-  statement_id_prefix = "ExecuteByAPI"
-  action              = "lambda:InvokeFunction"
-  function_name       = aws_lambda_function.update_user_lambda.function_name
-  principal           = "apigateway.amazonaws.com"
-  source_arn          = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/PATCH/update"
+resource "aws_lambda_permission" "apigw_invoke_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.update_user_lambda.arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn          = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/PATCH/update"
 }
-
-
