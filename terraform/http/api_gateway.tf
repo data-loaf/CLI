62998382f5056534bac7b0d2e528b9bb474f6bbf
Brandon Corey <bcorey3660@gmail.com>
2024-04-30 03:55:34 -0400

    added stage to api gateway

--- terraform/http/api_gateway.tf
+++ terraform/http/api_gateway.tf
@@ -60,22 +60,24 @@ resource "aws_iam_role" "api_gateway_role" {
 
 resource "aws_api_gateway_integration" "events_kinesis_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id            = aws_api_gateway_resource.events.id
+  resource_id             = aws_api_gateway_resource.events.id
   http_method             = aws_api_gateway_method.post_events.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:kinesis:action/PutRecord"
   credentials             = aws_iam_role.api_gateway_role.arn
+
 }
 
 resource "aws_api_gateway_integration" "users_kinesis_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id            = aws_api_gateway_resource.users.id
+  resource_id             = aws_api_gateway_resource.users.id
   http_method             = aws_api_gateway_method.post_users.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:kinesis:action/PutRecord"
   credentials             = aws_iam_role.api_gateway_role.arn
+
 }
 
 resource "aws_api_gateway_integration" "update_user_lambda_integration" {
@@ -125,4 +127,22 @@ resource "aws_api_gateway_method_response" "users_method_response_200" {
   resource_id = aws_api_gateway_resource.users.id
   http_method = aws_api_gateway_method.post_users.http_method
   status_code = "200"
+}
+
+resource "aws_api_gateway_stage" "bake_stage" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  deployment_id = aws_api_gateway_deployment.api_gateway_deployment.id
+  stage_name = "bake-stage"
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  depends_on      = [aws_api_gateway_rest_api.api_gateway]
+  rest_api_id     = aws_api_gateway_rest_api.api_gateway.id
+  stage_name      = "bake-stage"
+  description     = "Automatically deployed bake stage"
+}
+
+
+output "api_gateway_url" {
+  value = aws_api_gateway_stage.bake_stage.invoke_url
 }
