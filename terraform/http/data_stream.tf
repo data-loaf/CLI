3765bc045f92820b3764ed69f599c41772ce7d2f
Brandon Corey <bcorey3660@gmail.com>
2024-05-03 03:38:48 -0400

    move api gateway policy to put into data stream into api_gateway.tf
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- terraform/http/data_stream.tf
+++ terraform/http/data_stream.tf
@@ -19,29 +19,6 @@ locals {
   }
 }
 
-resource "aws_iam_policy" "kinesis_policy" {
-  name        = "kinesis_put_record_policy"
-  description = "Policy to allow PutRecord action on the Kinesis streams"
-  
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = "kinesis:PutRecord"
-      Resource = [
-        "arn:aws:kinesis:us-east-1:240398672806:stream/loaf-user-kinesis-stream",
-        "arn:aws:kinesis:us-east-1:240398672806:stream/loaf-event-kinesis-stream"
-      ]
-    }]
-  })
-}
-
-resource "aws_iam_policy_attachment" "api_gateway_kinesis_attachment" {
-  name       = "api_gateway_kinesis_attachment"
-  roles      = [aws_iam_role.api_gateway_role.name]
-  policy_arn = aws_iam_policy.kinesis_policy.arn
-}
-
 resource "aws_kinesis_stream" "loaf_stream" {
   for_each         = local.data_streams
   name             = each.value.name
